b52634c19e0b938e521aa7e388329c8a573653ec
Channing Tyner <channingtyner@gmail.com>
2018-10-01 17:38:06 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+  return
+string.toUpperCase()
+}
+
+function whisper(string) {
+  return 
+  string.toLowerCase()
+  
+}
+
+function logShout(string) {
+  return 
+  string.toUpperCase()
+  console.log(logShout())
+  
+}
+
+function logWhisper(string) {
+  return 
+  string.toLowerCase()
+  console.log(logWhisper())
+  
+}
+
+function sayHiToGrandma(string) {
+  
+ return  
+}
